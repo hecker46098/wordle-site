4040d3c7dc47be7b84a03f17c8d16b0280048904
Benjamin Johnson <ben332004@gmail.com>
2022-07-18 00:56:01 -0700

    Add Bootstrap and link to source code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="src-button">
+      <a className="btn btn-large btn-success mb-5" href="https://github.com/benjaminJohnson2204/wordle-site" rel="noopener noreferrer" target="_blank">
+            View Source Code
+        </a>
+        </div>
         <div className="header-text">
           Ben's Wordle Game
         </div>
